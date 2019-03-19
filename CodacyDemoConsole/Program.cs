aac6712797dbb201bd98df7789653eb3d83f2701
Ben Janis <projectnewgear@gmail.com>
2019-03-19 00:01:47 -0500

    commit 2

---
+++ CodacyDemoConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CodacyDemoClassLibrary;
+
+namespace CodacyDemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patient pat1 = new Patient("Beleren", "Jace", Convert.ToDateTime("6-3-1988"));
+            Patient pat2 = new Patient("Nalaar", "Chandra", Convert.ToDateTime("1-1-2000"));
+
+            pat1 = pat2;
+        }
+    }
+}
